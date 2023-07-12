2fa6d177702fb74134eb1fdc0d1565fb19315d29
SzymKam <szymon15kaminski@gmail.com>
2023-07-12 13:15:55 +0200

    feat: update event model

--- src/events/models.py
+++ src/events/models.py
@@ -9,5 +9,5 @@ class Event(models.Model):
     name = models.CharField(max_length=100),
     description = models.CharField(max_length=200, blank=True, null=True),
     localization = models.CharField(max_length=50, blank=True, null=True),
-    status = models.CharField(choices=EVENT_STATUS, default="In progress"),
+    status = models.CharField(choices=EVENT_STATUS, default="Preparing"),
     end_date = models.DateField(blank=True, null=True),
