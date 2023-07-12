4fc68b961dffd97d43032fbb817cbda2a20233cd
SzymKam <szymon15kaminski@gmail.com>
2023-07-12 15:04:12 +0200

    feat: add Patient model

--- src/patients/models.py
+++ src/patients/models.py
@@ -2,8 +2,14 @@ from django.db import models
 from datetime import datetime
 
 from src.events.models import Event
-
+from src.treatment.models import Treatment
 from .constants import SEX, BED_CHOICE
+
+
+class AuthorizedPerson(models.Model):
+    name = models.CharField(max_length=50, blank=True, help_text="Person name")
+    surname = models.CharField(max_length=50, blank=True, help_text="Person surname")
+    phone = models.IntegerField(blank=True, null=True, help_text="Person phone number")
 
 
 class Patient(models.Model):
@@ -18,6 +24,6 @@ class Patient(models.Model):
     e_mail = models.EmailField(blank=True, null=True, help_text="Patient e-mail")
     additional_info = models.TextField(blank=True, help_text="Additional info about patient")
     bed_number = models.CharField(choices=BED_CHOICE, blank=True, help_text="Bed number in hospital")
-    auth_person = models.ForeignKey(AuthPerson, default="No authorized person", on_delete=models.SET("No authorized person"), blank=True, null=True, help_text="Set authorized person")
-    event = models.ForeignKey(Event, default="No event assigned", on_delete=models.SET("No event assigned"), help_text="Set event")
-    treatment = models.ForeignKey(Treatment, default="No treatment added", on_delete=models.SET("No treatment"), help_text="Add treatment")
+    authorized_person = models.OneToOneField(AuthorizedPerson, default="No authorized person", on_delete=models.SET("No authorized person"), blank=True, null=True, help_text="Set authorized person", related_name="patient",)
+    event = models.ForeignKey(Event, default="No event assigned", on_delete=models.SET("No event assigned"), help_text="Set event", related_name="patient",)
+    treatment = models.OneToOneField(Treatment, default="No treatment added", on_delete=models.SET("No treatment"), help_text="Add treatment", related_name="patient",)
