cfa5f4888a43ed0b4b3f15cc459b299a1787e11b
SzymKam <szymon15kaminski@gmail.com>
2023-07-14 00:09:01 +0200

    fix: invalid model fields

--- src/patients/models.py
+++ src/patients/models.py
@@ -13,17 +13,45 @@ class AuthorizedPerson(models.Model):
 
 
 class Patient(models.Model):
-    admission_date = models.DateTimeField(default=datetime.now(), help_text="Date of patient admission")
+    admission_date = models.DateTimeField(
+        default=datetime.now(), help_text="Date of patient admission"
+    )
     name = models.CharField(max_length=50, blank=True, help_text="Patient name")
     surname = models.CharField(max_length=50, blank=True, help_text="Patient surname")
     PESEL = models.IntegerField(blank=True, null=True, help_text="Patient PESEL number")
     birth_date = models.DateField(blank=True, null=True, help_text="Patient birth date")
-    sex = models.CharField(choices=SEX, blank=True, help_text="Patient sex", max_length=6)
+    sex = models.CharField(
+        choices=SEX, blank=True, help_text="Patient sex", max_length=6
+    )
     address = models.CharField(blank=True, help_text="Patient address", max_length=100)
     phone = models.IntegerField(blank=True, null=True, help_text="Patient phone number")
-    e_mail = models.EmailField(blank=True, null=True, help_text="Patient e-mail")
-    additional_info = models.TextField(blank=True, help_text="Additional info about patient")
-    bed_number = models.CharField(choices=BED_CHOICE, blank=True, help_text="Bed number in hospital", max_length=6)
-    authorized_person = models.OneToOneField(AuthorizedPerson, default="No authorized person", on_delete=models.SET("No authorized person"), blank=True, null=True, help_text="Set authorized person", related_name="patient",)
-    event = models.ForeignKey(Event, default="No event assigned", on_delete=models.SET("No event assigned"), help_text="Set event", related_name="patient",)
-    treatment = models.OneToOneField(Treatment, default="No treatment added", on_delete=models.SET("No treatment"), help_text="Add treatment", related_name="patient",)
+    email = models.EmailField(blank=True, null=True, help_text="Patient e-mail")
+    additional_info = models.TextField(
+        blank=True, help_text="Additional info about patient"
+    )
+    bed_number = models.CharField(
+        choices=BED_CHOICE, blank=True, help_text="Bed number in hospital", max_length=6
+    )
+    authorized_person = models.OneToOneField(
+        AuthorizedPerson,
+        default="No authorized person",
+        on_delete=models.SET("No authorized person"),
+        blank=True,
+        null=True,
+        help_text="Set authorized person",
+        related_name="patient",
+    )
+    event = models.ForeignKey(
+        Event,
+        default="No event assigned",
+        on_delete=models.SET("No event assigned"),
+        help_text="Set event",
+        related_name="patient",
+    )
+    treatment = models.OneToOneField(
+        Treatment,
+        default="No treatment added",
+        on_delete=models.SET("No treatment"),
+        help_text="Add treatment",
+        related_name="patient",
+    )
