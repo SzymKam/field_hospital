5cc50b58156a927969369737fd01ca0300fad474
SzymKam <szymon15kaminski@gmail.com>
2023-07-13 21:13:07 +0200

    feat: add new fields into models

--- src/patients/models.py
+++ src/patients/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from datetime import datetime
 
-from src.events.models import Event
-from src.treatment.models import Treatment
+from events.models import Event
+from treatment.models import Treatment
 from .constants import SEX, BED_CHOICE
 
 
@@ -16,14 +16,14 @@ class Patient(models.Model):
     admission_date = models.DateTimeField(default=datetime.now(), help_text="Date of patient admission")
     name = models.CharField(max_length=50, blank=True, help_text="Patient name")
     surname = models.CharField(max_length=50, blank=True, help_text="Patient surname")
-    PESEL = models.IntegerField(max_length=11, blank=True, null=True, help_text="Patient PESEL number")
+    PESEL = models.IntegerField(blank=True, null=True, help_text="Patient PESEL number")
     birth_date = models.DateField(blank=True, null=True, help_text="Patient birth date")
-    sex = models.CharField(choices=SEX, blank=True, help_text="Patient sex")
-    address = models.CharField(blank=True, help_text="Patient address")
+    sex = models.CharField(choices=SEX, blank=True, help_text="Patient sex", max_length=6)
+    address = models.CharField(blank=True, help_text="Patient address", max_length=100)
     phone = models.IntegerField(blank=True, null=True, help_text="Patient phone number")
     e_mail = models.EmailField(blank=True, null=True, help_text="Patient e-mail")
     additional_info = models.TextField(blank=True, help_text="Additional info about patient")
-    bed_number = models.CharField(choices=BED_CHOICE, blank=True, help_text="Bed number in hospital")
+    bed_number = models.CharField(choices=BED_CHOICE, blank=True, help_text="Bed number in hospital", max_length=6)
     authorized_person = models.OneToOneField(AuthorizedPerson, default="No authorized person", on_delete=models.SET("No authorized person"), blank=True, null=True, help_text="Set authorized person", related_name="patient",)
     event = models.ForeignKey(Event, default="No event assigned", on_delete=models.SET("No event assigned"), help_text="Set event", related_name="patient",)
     treatment = models.OneToOneField(Treatment, default="No treatment added", on_delete=models.SET("No treatment"), help_text="Add treatment", related_name="patient",)
--- src/treatment/models.py
+++ src/treatment/models.py
@@ -1,10 +1,44 @@
 from django.db import models
+from datetime import datetime
+from .constants import MEDICAL_QUALIFICATIONS, DRUG_AND_FLUID_CHOICES, DRUG_DOSAGE_FORM, FLUID_VOLUME, VITAL_SIGN_NAME
+
+
+class MedicalStaff(models.Model):
+    name = models.CharField(blank=True, help_text="Medic name", max_length=50)
+    surname = models.CharField(blank=True, help_text="Medic surname", max_length=50)
+    medical_qualifications = models.CharField(choices=MEDICAL_QUALIFICATIONS,
+                                              default="First aid",
+                                              help_text="Qualifications of medic", max_length=20)
+
+
+class Drug(models.Model):
+    name = models.CharField(choices=DRUG_AND_FLUID_CHOICES, blank=True, help_text="Add new drug", max_length=40)
+    dosage_form = models.CharField(choices=DRUG_DOSAGE_FORM, blank=True, help_text="Dosage form of drug", max_length=20)
+    volume = models.CharField(choices=FLUID_VOLUME, blank=True, help_text="Volume of fluid", max_length=20)
+
+
+class VitalSign(models.Model):
+    name = models.CharField(choices=VITAL_SIGN_NAME, help_text="Choose parameter", max_length=15)
+    value = models.CharField(help_text="Enter value", max_length=20)
+    datetime = models.DateTimeField(datetime.now())
+    additional_info = models.TextField()
 
 
 class Treatment(models.Model):
     interview = models.TextField(blank=True, help_text="Place for medical interview")
+    vital_sign = models.ForeignKey(VitalSign,
+                                   blank=True,
+                                   help_text="Vital signs of patient",
+                                   max_length=50,
+                                   on_delete=models.CASCADE)
+    drugs = models.ForeignKey(Drug,
+                              blank=True,
+                              help_text="Medications given to the patient",
+                              max_length=50,
+                              on_delete=models.CASCADE)
     description = models.TextField(blank=True, help_text="Place for patient description")
-
-
-    diagnosis = models.CharField(help_text="Diagnose")
-
+    diagnosis = models.CharField(blank=True, help_text="Diagnose", max_length=50)  # todo maybe add icd10 library??
+    additional_info = models.TextField(blank=True, help_text="Additional info about patient treatment")
+    medical_staff = models.ForeignKey(MedicalStaff,
+                                      on_delete=models.SET("No staff assigned"),
+                                      help_text="Add medic to patient")
