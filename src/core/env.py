9fbab48a70ce82546274a939892088729115b101
SzymKam <szymon15kaminski@gmail.com>
2023-07-16 15:47:35 +0200

    fix: delete variable

--- src/core/env.py
+++ src/core/env.py
@@ -2,11 +2,6 @@ import environ
 from django.core.management.utils import get_random_secret_key
 
 env = environ.Env(
-    DEBUG=(bool, True),
-    # SECRET_KEY=(str, get_random_secret_key()),
-    SECRET_KEY=(
-        str,
-        "django-insecure-%9nd24mrhy$8vgm2s5pga%h_^quy^xmxr^khbo_^s8zokuv1ky",
-    ),
-    MYPY_DJANGO_CONFIG=(str, "./mypy.ini"),
+    DEBUG=(bool, False),
+    SECRET_KEY=(str, get_random_secret_key()),
 )
