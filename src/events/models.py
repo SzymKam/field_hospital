66235e876e14f1753ec8a66b4442bcd539b1e388
SzymKam <szymon15kaminski@gmail.com>
2023-07-16 23:28:27 +0200

    feat/fix: add str, and fix fields in model

--- src/events/models.py
+++ src/events/models.py
@@ -4,9 +4,18 @@ from .constants import EVENT_STATUS
 
 
 class Event(models.Model):
-    start_date = (models.DateField(auto_now_add=True),)
-    name = (models.CharField(max_length=100),)
-    description = (models.CharField(max_length=200, blank=True, null=True),)
-    localization = (models.CharField(max_length=50, blank=True, null=True),)
-    status = (models.CharField(choices=EVENT_STATUS, default="Preparing"),)
+    start_date = models.DateField(auto_now=True)
+    name = models.CharField(
+        max_length=100, default="Default event", help_text="Enter event name"
+    )
+    description = models.CharField(
+        max_length=200, blank=True, null=True, help_text="Enter description"
+    )
+    localization = models.CharField(
+        max_length=50, blank=True, null=True, help_text="Enter localization"
+    )
+    status = models.CharField(choices=EVENT_STATUS, default="Preparing", max_length=20)
     end_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
