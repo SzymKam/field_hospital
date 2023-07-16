c83529a7f5e0d48a28c1c8e62a45bce038d4f51e
SzymKam <szymon15kaminski@gmail.com>
2023-07-16 12:43:50 +0200

    fix: update file

--- src/core/env.py
+++ src/core/env.py
@@ -3,5 +3,9 @@ from django.core.management.utils import get_random_secret_key
 
 env = environ.Env(
     DEBUG=(bool, True),
-    SECRET_KEY=(str, get_random_secret_key()),
+    # SECRET_KEY=(str, get_random_secret_key()),
+    SECRET_KEY=(
+        str,
+        "django-insecure-%9nd24mrhy$8vgm2s5pga%h_^quy^xmxr^khbo_^s8zokuv1ky",
+    ),
 )
