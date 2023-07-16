ed9b293ffb74e62f747b640836f36aca774b3202
SzymKam <szymon15kaminski@gmail.com>
2023-07-16 15:48:15 +0200

    feat: add typing into methods

--- src/events/views.py
+++ src/events/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from django.contrib import messages
 from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
+from typing import Any
 from .models import Event
 from .forms import EventForm
 from patients.models import Patient
@@ -11,13 +18,13 @@ class AllEventView(ListView):
     template_name = ""
     queryset = Event.objects.all()
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs: dict[str, Any]) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
-        context["title"] = f"Events list"
+        context["title"] = "Events list"
         context["subtitle"] = self.object
         return context
 
-    #todo - add login required
+    # todo - add login required
 
 
 class DetailEventView(DetailView):
@@ -25,11 +32,11 @@ class DetailEventView(DetailView):
     queryset = Event.objects.all()
 
     @staticmethod
-    def get_all_patients_from_event(event_id):
+    def get_all_patients_from_event(event_id: int) -> list[Patient]:
         patients = Patient.objects.filter(event=event_id)
         return patients
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs: dict[str, Any]) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context["patients"] = self.get_all_patients_from_event(event_id=self.object.id)
         context["title"] = f"Event {self.object.name}"
@@ -45,13 +52,13 @@ class CreateEventView(CreateView):
     form_class = EventForm
     queryset = Event.objects.all()
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs: dict[str, Any]) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context["title"] = "Create event"
         context["subtitle"] = "Creating new event"
         return context
 
-    #todo add permissions, messages, login_url, succes_url, override form_valid for succes message
+    # todo add permissions, messages, login_url, succes_url, override form_valid for succes message
 
 
 class UpdateEventView(UpdateView):
@@ -59,7 +66,7 @@ class UpdateEventView(UpdateView):
     template_name = ""
     form_class = EventForm
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs: dict[str, Any]) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context["title"] = "Update event"
         context["subtitle"] = "Updating new event"
@@ -72,7 +79,7 @@ class DeleteEventView(DeleteView):
     model = Event
     template_name = ""
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs: dict[str, Any]) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context["title"] = "Delete event"
         context["subtitle"] = "Deleting new event"
