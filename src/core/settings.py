4d86bbaa3620fd876b17980f33faad2aced23148
SzymKam <szymon15kaminski@gmail.com>
2023-07-16 15:46:41 +0200

    feat: change dir in environ

--- src/core/settings.py
+++ src/core/settings.py
@@ -9,18 +9,18 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 import environ
 from pathlib import Path
 from .env import env
 
 
-environ.Env.read_env()
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+ENV_DIR = BASE_DIR.parent
+
+environ.Env.read_env(os.path.join(ENV_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
