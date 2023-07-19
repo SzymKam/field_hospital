7c6aa1555b2b28e87871fea9091d486452d2abbf
SzymKam <szymon15kaminski@gmail.com>
2023-07-20 01:27:38 +0200

    feat: add urls for new views

--- src/events/urls.py
+++ src/events/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 
-from .views import AllEventView, CreateEventView, DetailEventView
+from .views import (
+    AllActiveEventView,
+    AllInactiveEventView,
+    CreateEventView,
+    DetailEventView,
+    UpdateEventView,
+    DeleteEventView,
+)
 
 urlpatterns = [
-    path("", AllEventView.as_view(), name="all-events"),
+    path("", AllActiveEventView.as_view(), name="all-events"),
     path("create-event/", CreateEventView.as_view(), name="add-events"),
     path("detail-event/<int:pk>", DetailEventView.as_view(), name="detail-events"),
+    path("update-event/<int:pk>", UpdateEventView.as_view(), name="update-events"),
+    path("inactive-event/", AllInactiveEventView.as_view(), name="complete-events"),
+    path("delete-events/<int:pk>", DeleteEventView.as_view(), name="delete-events"),
 ]
