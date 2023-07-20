7e86ecaf605a34625c588a4ffbaba0a09de25dd8
SzymKam <szymon15kaminski@gmail.com>
2023-07-20 12:14:56 +0200

    feat: add new form for end_date when closing event and add wigets for date

--- src/events/forms.py
+++ src/events/forms.py
@@ -1,8 +1,17 @@
 from django.forms import ModelForm
+from django.forms.widgets import DateInput
 from .models import Event
 
 
 class EventForm(ModelForm):
     class Meta:
+        widgets = {"start_date": DateInput(attrs={"type": "date"})}
         model = Event
-        fields = ["name", "description", "localization", "status"]
+        fields = ["name", "start_date", "description", "localization", "status"]
+
+
+class CloseEventForm(ModelForm):
+    class Meta:
+        widgets = {"end_date": DateInput(attrs={"type": "date"})}
+        model = Event
+        fields = ["end_date"]
