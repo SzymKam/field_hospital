b09764205b8a7577842f392ef640b60865d64100
SzymKam <szymon15kaminski@gmail.com>
2023-07-20 12:16:29 +0200

    feat: add urls for new views

--- src/events/urls.py
+++ src/events/urls.py
@@ -7,13 +7,22 @@ from .views import (
     DetailEventView,
     UpdateEventView,
     DeleteEventView,
+    CloseRestoreEventView,
 )
 
 urlpatterns = [
     path("", AllActiveEventView.as_view(), name="all-events"),
-    path("create-event/", CreateEventView.as_view(), name="add-events"),
+    path("add-event/", CreateEventView.as_view(), name="add-events"),
     path("detail-event/<int:pk>", DetailEventView.as_view(), name="detail-events"),
     path("update-event/<int:pk>", UpdateEventView.as_view(), name="update-events"),
     path("inactive-event/", AllInactiveEventView.as_view(), name="complete-events"),
-    path("delete-events/<int:pk>", DeleteEventView.as_view(), name="delete-events"),
+    path("delete-event/<int:pk>", DeleteEventView.as_view(), name="delete-events"),
+    path(
+        "close-event/<int:pk>", CloseRestoreEventView.close_event, name="close-events"
+    ),
+    path(
+        "restore-event/<int:pk>",
+        CloseRestoreEventView.restore_event,
+        name="restore-events",
+    ),
 ]
