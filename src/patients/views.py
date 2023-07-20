4fa14bb4ca0ea51bf71cbc2532fb6ee45b9442e9
SzymKam <szymon15kaminski@gmail.com>
2023-07-20 16:04:09 +0200

    feat: add create patient button

--- src/patients/views.py
+++ src/patients/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView
+from typing import Any
+from .models import Patient
+from .forms import PatientForm
 
-# Create your views here.
+
+class CreatePatientView(CreateView):
+    model = Patient
+    template_name = "patients/patients-new.html"
+    form_class = PatientForm
+    queryset = Patient.objects.all()
+    success_url = reverse_lazy("all-events")
+
+    # def get_event_id(self, request):
+    #     self.event_id = request.resolver_match.kwargs['event']
+    #     print(self.event_id)
+    #     return self.event_id
+
+    def get_context_data(self, **kwargs: dict[str, Any]) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Add patient"
+        return context
+
+    # @staticmethod
+    # def get_success_url(request):
+    #     return reverse_lazy("detail-events", kwargs={'pk': 4})
