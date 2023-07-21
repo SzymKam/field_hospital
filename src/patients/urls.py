38322752cf290eec1bcf0f5f745904107e161a15
SzymKam <szymon15kaminski@gmail.com>
2023-07-21 19:00:15 +0200

    feat: add and update urls for new views

--- src/patients/urls.py
+++ src/patients/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from .views import CreatePatientView
+from .views import CreatePatientView, DetailPatientView
 
 urlpatterns = [
-    path("<int:event>/add-patient", CreatePatientView.as_view(), name="add-patient"),
+    path(
+        "event:<int:event>/add-patient",
+        CreatePatientView.create_patient,
+        name="add-patient",
+    ),
+    path(
+        "event:<int:event>/patient:<int:pk>",
+        DetailPatientView.as_view(),
+        name="detail-patient",
+    ),
 ]
