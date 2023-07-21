a2b6eda5d91467dde0f65cff4644d8fd04a7c1a8
SzymKam <szymon15kaminski@gmail.com>
2023-07-21 18:59:17 +0200

    feat: add new form for detail patient view

--- src/patients/forms.py
+++ src/patients/forms.py
@@ -4,10 +4,27 @@ from django.forms.widgets import DateInput
 
 
 class PatientForm(ModelForm):
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.fields['event'].widget.attrs['disabled'] = True
+
     class Meta:
         widgets = {"birth_date": DateInput(attrs={"type": "date"})}
         model = Patient
-        exclude = ("authorized_person", "treatment")
+        exclude = (
+            "authorized_person",
+            "treatment",
+        )
+
+
+class DetailPatientForm(ModelForm):
+    class Meta:
+        widgets = {"birth_date": DateInput(attrs={"type": "date"})}
+        model = Patient
+        exclude = (
+            "status",
+            "event",
+        )
 
 
 class AuthorizedPersonForm(ModelForm):
