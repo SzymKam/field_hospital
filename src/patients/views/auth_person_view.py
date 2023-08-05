ecbe6a49297b2a22e55b80bbb48e68e4023ec130
SzymKam <szymon15kaminski@gmail.com>
2023-08-05 23:33:33 +0200

    feat: add view for update auth person

--- src/patients/views/auth_person_view.py
+++ src/patients/views/auth_person_view.py
@@ -41,4 +41,23 @@ class CreateAuthPersonView(View):
 
 
 class UpdateAuthPersonView(UpdateView):
-    pass
+    template_name = "patients/auth-person-update.html"
+    model = AuthorizedPerson
+    form_class = AuthorizedPersonForm
+
+    def get_data(self) -> dict:
+        event = get_object_or_404(klass=Event, pk=self.kwargs["event"])
+        patient = get_object_or_404(klass=Patient, pk=self.kwargs["patient"])
+        return {"event": event, "patient": patient}
+
+    def get_context_data(self, **kwargs: dict[str, Any]) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Auth person update"
+        context.update(self.get_data())
+        return context
+
+    def get_success_url(self):
+        return reverse_lazy(
+            "detail-patient",
+            kwargs={"event": self.kwargs["event"], "pk": self.kwargs["patient"]},
+        )
