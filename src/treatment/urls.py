f8e8a64ada500ef963b312aae08851df26d9c35d
SzymKam <szymon15kaminski@gmail.com>
2023-08-06 19:59:52 +0200

    feat: add urls for detail and create treatment

--- src/treatment/urls.py
+++ src/treatment/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 
-from .views import CreateTreatmentView
+from .views.treatment_views import CreateTreatmentView, DetailTreatmentView
 
 
 urlpatterns = [
-    path("treatment/", CreateTreatmentView.as_view(), name="treatment"),
+    path(
+        "events/<int:event>/patients/<int:patient>/treatment/create",
+        CreateTreatmentView.as_view(),
+        name="create-treatment",
+    ),
+    path(
+        "events/<int:event>/patients/<int:patient>/treatment/<int:pk>",
+        DetailTreatmentView.as_view(),
+        name="detail-treatment",
+    ),
 ]
