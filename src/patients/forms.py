156ddec81236e4162dd689c4ba8a0758ed58481e
SzymKam <szymon15kaminski@gmail.com>
2023-08-06 23:50:12 +0200

    fis/feat: add initial values for surname - when patient has no name

--- src/patients/forms.py
+++ src/patients/forms.py
@@ -1,12 +1,29 @@
 from django.forms import ModelForm
 from .models import Patient, AuthorizedPerson
 from django.forms.widgets import DateInput, DateTimeInput
+from datetime import datetime
 
 
 class PatientForm(ModelForm):
     def __init__(self, *args, **kwargs):
+        self.initial_form_data = f"NN {datetime.now().strftime('%m-%d-%Y %H:%M:%S')}"
+        if "initial" not in kwargs:
+            kwargs["initial"] = {}
+        if "surname" not in kwargs["initial"]:
+            kwargs["initial"]["surname"] = self.initial_form_data
+
         super().__init__(*args, **kwargs)
         self.fields["event"].widget.attrs["disabled"] = True
+
+    def save(self, commit=True):
+        instance = super(PatientForm, self).save(commit=False)
+        if instance.surname == "":
+            instance.surname = self.initial_form_data
+
+        if commit:
+            instance.save()
+
+        return instance
 
     class Meta:
         widgets = {
