d603f0b0575148b54788a1f93ee6b625e59bda99
SzymKam <szymon15kaminski@gmail.com>
2023-08-06 19:53:59 +0200

    feat: add migrations

--- src/treatment/views/treatment_views.py
+++ src/treatment/views/treatment_views.py
@@ -9,20 +9,27 @@ from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 from patients.models import Patient
+from events.models import Event
 
-from .forms import TreatmentForm
-from .models import Treatment
+from treatment.forms import TreatmentForm, TreatmentCreateForm
+from treatment.models import Treatment
 
 
 class CreateTreatmentView(CreateView):
     model = Treatment
     template_name = "treatment/treatment-new.html"
-    form_class = TreatmentForm
+    form_class = TreatmentCreateForm
     queryset = Treatment.objects.all()
+
+    def get_data(self) -> dict:
+        event = get_object_or_404(klass=Event, pk=self.kwargs["event"])
+        patient = get_object_or_404(klass=Patient, pk=self.kwargs["patient"])
+        return {"event": event, "patient": patient}
 
     def get_context_data(self, **kwargs: dict[str, Any]) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context["title"] = "Treatment"
+        context.update(self.get_data())
         return context
 
     # todo add permissions, login_url
