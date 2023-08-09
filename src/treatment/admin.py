9b8e17f0e68914e14ae082643930a4f4308f7651
SzymKam <szymon15kaminski@gmail.com>
2023-08-09 23:55:46 +0200

    feat: add Drug into admin panel

--- src/treatment/admin.py
+++ src/treatment/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import MedicalStaff
+from .models import MedicalStaff, Drug
 
 
 @admin.register(MedicalStaff)
 class MedicalStaffAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Drug)
+class DrugAdmin(admin.ModelAdmin):
     pass
