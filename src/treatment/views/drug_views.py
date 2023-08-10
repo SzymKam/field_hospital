aae0dc26c9a8e2621590bf293b41d92b06070ee1
SzymKam <szymon15kaminski@gmail.com>
2023-08-10 16:02:27 +0200

    feat: add update drug view

--- src/treatment/views/drug_views.py
+++ src/treatment/views/drug_views.py
@@ -3,8 +3,6 @@ from typing import Any
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView
-from patients.models import Patient
-from events.models import Event
 
 from treatment.models.treatment_model import Treatment
 from treatment.models.drug_model import Drug
@@ -36,4 +34,23 @@ class CreateDrugView(CreateView):
         return reverse_lazy(
             "detail-treatment",
             kwargs={"event": self.kwargs["event"], "patient": self.kwargs["patient"], "pk": self.kwargs["pk"]},
+        )
+
+
+class UpdateDrugView(UpdateView):
+    model = Drug
+    template_name = "treatment/treatment-edit-drug.html"
+    form_class = DrugForm
+    queryset = Drug.objects.all()
+
+    def get_context_data(self, **kwargs: dict[str, Any]) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Edit drug"
+        context.update(get_event_and_patient(event_id=self.kwargs["event"], patient_id=self.kwargs["patient"]))
+        return context
+
+    def get_success_url(self) -> dict[str, Any]:
+        return reverse_lazy(
+            "detail-treatment",
+            kwargs={"event": self.kwargs["event"], "patient": self.kwargs["patient"], "pk": self.kwargs["treatment"]},
         )
