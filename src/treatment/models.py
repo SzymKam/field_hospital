c97579a7e41fc9ecd0ed6a02c8d6d54d2b5f3c73
SzymKam <szymon15kaminski@gmail.com>
2023-08-10 11:52:51 +0200

    fix/feat: remove vital_sign field from treatment model and add treatment FK field into vital sign model

--- src/treatment/models.py
+++ src/treatment/models.py
@@ -4,7 +4,6 @@ from .constants import (
     MEDICAL_QUALIFICATIONS,
     DRUG_AND_FLUID_CHOICES,
     DRUG_DOSAGE_FORM,
-    FLUID_VOLUME,
     VITAL_SIGN_NAME,
     UNIT_CHOICES,
 )
@@ -24,27 +23,12 @@ class MedicalStaff(models.Model):
         return f"{self.name} {self.surname} - {self.medical_qualifications}"
 
 
-class VitalSign(models.Model):
-    name = models.CharField(choices=VITAL_SIGN_NAME, help_text="Choose parameter", max_length=15)
-    value = models.CharField(help_text="Enter value", max_length=20)
-    datetime = models.DateTimeField(auto_now_add=True)
-    additional_info = models.TextField()
-
-
 class Treatment(models.Model):
     """
     Represents a patient's treatment record.
     """
 
     interview = models.TextField(blank=True, help_text="Place for medical interview")
-    vital_sign = models.ForeignKey(
-        VitalSign,
-        blank=True,
-        null=True,
-        help_text="Vital signs of patient",
-        max_length=50,
-        on_delete=models.CASCADE,
-    )
     description = models.TextField(blank=True, help_text="Place for patient description")
     diagnosis = models.CharField(blank=True, help_text="Diagnose?", max_length=50)
     medical_staff = models.ForeignKey(
@@ -79,3 +63,18 @@ class Drug(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+
+
+class VitalSign(models.Model):
+    name = models.CharField(choices=VITAL_SIGN_NAME, help_text="Choose parameter", max_length=15)
+    value = models.CharField(help_text="Enter value", max_length=20)
+    datetime = models.DateTimeField(auto_now_add=True)
+    additional_info = models.TextField()
+    treatment = models.ForeignKey(
+        Treatment,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        help_text="Add vital sign",
+        related_name="vital_sign",
+    )
