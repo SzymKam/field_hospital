19d2543af1d7bb3e28a5ce692e5800b535d04b29
SzymKam <szymon15kaminski@gmail.com>
2023-08-11 16:58:46 +0200

    fix/feat: update UpdateDrugView and add DeleteDrug View

--- src/treatment/views/drug_views.py
+++ src/treatment/views/drug_views.py
@@ -2,7 +2,7 @@ from typing import Any
 
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView, DeleteView
 
 from treatment.models.treatment_model import Treatment
 from treatment.models.drug_model import Drug
@@ -46,6 +46,23 @@ class UpdateDrugView(UpdateView):
     def get_context_data(self, **kwargs: dict[str, Any]) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context["title"] = "Edit drug"
+        context.update(get_event_and_patient(event_id=self.kwargs["event"], patient_id=self.kwargs["patient"]))
+        return context
+
+    def get_success_url(self) -> dict[str, Any]:
+        return reverse_lazy(
+            "detail-treatment",
+            kwargs={"event": self.kwargs["event"], "patient": self.kwargs["patient"], "pk": self.kwargs["treatment"]},
+        )
+
+
+class DeleteDrugView(DeleteView):
+    model = Drug
+    template_name = "treatment/treatment-delete-drug.html"
+
+    def get_context_data(self, **kwargs: dict[str, Any]) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Delete drug"
         context.update(get_event_and_patient(event_id=self.kwargs["event"], patient_id=self.kwargs["patient"]))
         return context
 
