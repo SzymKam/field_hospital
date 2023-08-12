6232a92012a5157bc96b8a7deac498977023f1b6
SzymKam <szymon15kaminski@gmail.com>
2023-08-12 21:46:48 +0200

    feat: add isrot into precommit

--- src/treatment/forms/vital_sign_form.py
+++ src/treatment/forms/vital_sign_form.py
@@ -1,8 +1,9 @@
-from treatment.models.vital_sign_model import VitalSign
 from django.forms import ModelForm
+
+from treatment.models.vital_sign_model import VitalSign
 
 
 class VitalSignForm(ModelForm):
     class Meta:
         model = VitalSign
-        fields = ["name", "value"]
+        fields = ["name", "value", "extra_value"]
