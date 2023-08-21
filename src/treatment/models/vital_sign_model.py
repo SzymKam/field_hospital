ed778abb523f15505705b65308d519967516abd3
SzymKam <szymon15kaminski@gmail.com>
2023-08-21 23:04:36 +0200

    fix: change extra value field

--- src/treatment/models/vital_sign_model.py
+++ src/treatment/models/vital_sign_model.py
@@ -7,9 +7,8 @@ from .treatment_model import Treatment
 class VitalSign(models.Model):
     name = models.CharField(choices=VITAL_SIGN_NAME, help_text="Choose parameter", max_length=15)
     value = models.IntegerField(help_text="Enter value")
-    extra_value = models.IntegerField(help_text="Additional value", blank=True, null=True)
+    extra_value = models.IntegerField(help_text="DIA BP value", blank=True, null=True)
     datetime = models.DateTimeField(auto_now_add=True)
-    additional_info = models.TextField()
     treatment = models.ForeignKey(
         Treatment,
         blank=True,
@@ -20,4 +19,6 @@ class VitalSign(models.Model):
     )
 
     def __str__(self):
+        if self.extra_value:
+            return f"{self.name} - {self.value}/{self.extra_value}"
         return f"{self.name} - {self.value}"
