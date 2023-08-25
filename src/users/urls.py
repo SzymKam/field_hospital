cbe44533e55045318cbf7656785b05a6638c74b5
SzymKam <szymon15kaminski@gmail.com>
2023-08-25 21:02:52 +0200

    fix: change login/logut classes into self creating classes

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
+from .views.login_logout_reset_views import StaffLogin, StaffLogout
 from .views.user_views import (
     CreateUserView,
     DeleteUserView,
     ListUserView,
-    LoginView,
-    LogoutView,
     UpdateUserView,
 )
 
@@ -18,6 +17,6 @@ urlpatterns = [
     path("list", ListUserView.as_view(), name="user-list"),
     path("update/<int:pk>", UpdateUserView.as_view(), name="user-update"),
     path("delete/<int:pk>", DeleteUserView.as_view(), name="user-delete"),
-    path("login", LoginView.as_view(template_name="users/login.html"), name="user-login"),
-    path("logout", LogoutView.as_view(), name="user-logout"),
+    path("login", StaffLogin.as_view(), name="user-login"),
+    path("logout", StaffLogout.as_view(), name="user-logout"),
 ]
