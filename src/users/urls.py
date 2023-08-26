d95510ef22d6b24cfe73e2098d8ece54809244c5
SzymKam <szymon15kaminski@gmail.com>
2023-08-26 11:34:28 +0200

    feat: add reset password urls

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 
-from .views.login_logout_reset_views import StaffLogin, StaffLogout
+from .views.login_logout_views import StaffLogin, StaffLogout
+from .views.password_reset_views import (
+    MyPasswordResetCompleteView,
+    MyPasswordResetConfirmView,
+    MyPasswordResetDoneView,
+    MyPasswordResetView,
+)
 from .views.user_views import (
     CreateUserView,
     DeleteUserView,
@@ -19,4 +25,20 @@ urlpatterns = [
     path("delete/<int:pk>", DeleteUserView.as_view(), name="user-delete"),
     path("login", StaffLogin.as_view(), name="user-login"),
     path("logout", StaffLogout.as_view(), name="user-logout"),
+    path("reset_password/", MyPasswordResetView.as_view(), name="reset-password"),
+    path(
+        "reset_password_sent/",
+        MyPasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>",
+        MyPasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset_password_complete/",
+        MyPasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
 ]
