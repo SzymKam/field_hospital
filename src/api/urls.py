8cf0a49e7eb983493dd1cf984c15cc6f5abdae5f
SzymKam <szymon15kaminski@gmail.com>
2023-08-28 14:02:29 +0200

    fix: update urls

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,11 +1,15 @@
 from rest_framework.routers import SimpleRouter
 
 from .views.event_view import EventViewSet
+from .views.medical_staff_view import MedicalStaffViewSet
+from .views.patient_view import PatientViewSet
 from .views.user_view import UserViewSet
 
 router = SimpleRouter()
-router.register(r"user", UserViewSet, basename="user")
-router.register(r"event", EventViewSet)
+router.register(r"user", UserViewSet, basename="api-user")
+router.register(r"event", EventViewSet, basename="api-event")
+router.register(r"medical-staff", MedicalStaffViewSet, basename="api-medical-staff")
+router.register(r"patient", PatientViewSet, basename="api-patient")
 
 
 urlpatterns = [] + router.urls
