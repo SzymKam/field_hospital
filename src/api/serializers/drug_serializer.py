892dbe78fbc83ce49f64013db0e86267a663a842
SzymKam <szymon15kaminski@gmail.com>
2023-08-29 11:31:18 +0200

    feat: add drug  serializer

---
+++ src/api/serializers/drug_serializer.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from treatment.models.drug_model import Drug
+
+
+class DrugSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Drug
+        fields = ["name", "dose", "unit", "dosage_form", "treatment"]
