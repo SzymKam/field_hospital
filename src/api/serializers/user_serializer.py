afdb1b64eea5641335453159cd76c5c7d862934b
SzymKam <szymon15kaminski@gmail.com>
2023-08-30 09:08:56 +0200

    fix/feat: add required fields and validate username in create user serializer. Add update model serializer with validation username and password. No fields required - optional. Add change password serializer with password validation

--- src/api/serializers/user_serializer.py
+++ src/api/serializers/user_serializer.py
@@ -6,9 +6,13 @@ from rest_framework.validators import UniqueValidator
 
 class CreateUserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
-    username = serializers.CharField(max_length=30)
+    username = serializers.CharField(
+        max_length=30, required=True, validators=[UniqueValidator(queryset=User.objects.all())]
+    )
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
 
     class Meta:
         model = User
@@ -24,6 +28,8 @@ class CreateUserSerializer(serializers.ModelSerializer):
         user = User.objects.create(
             username=validated_data["username"],
             email=validated_data["email"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
         )
 
         user.set_password(validated_data["password"])
@@ -33,6 +39,63 @@ class CreateUserSerializer(serializers.ModelSerializer):
 
 
 class UpdateUserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
+
     class Meta:
         model = User
         fields = ("username", "first_name", "last_name", "email", "user_permissions")
+
+    def validate_email(self, value):
+        user = self.context["request"].user
+        if User.objects.exclude(pk=user.pk).filter(email=value).exists():
+            raise serializers.ValidationError({"email": "This email is already in use."})
+        return value
+
+    def validate_username(self, value):
+        user = self.context["request"].user
+        if User.objects.exclude(pk=user.pk).filter(username=value).exists():
+            raise serializers.ValidationError({"username": "This username is already in use."})
+        return value
+
+    def update(self, instance, validated_data):
+        if validated_data.get("first_name"):
+            instance.first_name = validated_data["first_name"]
+        if validated_data.get("last_name"):
+            instance.last_name = validated_data["last_name"]
+        if validated_data.get("email"):
+            instance.last_name = validated_data["email"]
+        if validated_data.get("username"):
+            instance.last_name = validated_data["username"]
+        if validated_data.get("user_permissions"):
+            instance.last_name = validated_data["user_permissions"]
+
+        instance.save()
+        return instance
+
+
+class ChangePasswordUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+    old_password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ("old_password", "password", "password2")
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+
+        return attrs
+
+    def validate_old_password(self, value):
+        user = self.context["request"].user
+        if not user.check_password(value):
+            raise serializers.ValidationError({"old_password": "Old password is not correct"})
+        return value
+
+    def update(self, instance, validated_data):
+        instance.set_password(validated_data["password"])
+        instance.save()
+
+        return instance
