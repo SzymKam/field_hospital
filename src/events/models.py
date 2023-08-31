2955bcbb842c78d430e38ff93e5b653945c53e78
SzymKam <szymon15kaminski@gmail.com>
2023-08-31 12:23:14 +0200

    fix: change start_date field default value

--- src/events/models.py
+++ src/events/models.py
@@ -1,20 +1,14 @@
-from django.utils import timezone
 from django.db import models
+from django.utils import timezone
 
 from .constants import EVENT_STATUS
 
 
 class Event(models.Model):
-    start_date = models.DateField(default=timezone.now)
-    name = models.CharField(
-        max_length=100, default="Event", help_text="Enter event name"
-    )
-    description = models.CharField(
-        max_length=200, blank=True, null=True, help_text="Enter description"
-    )
-    localization = models.CharField(
-        max_length=50, blank=True, null=True, help_text="Enter localization"
-    )
+    start_date = models.DateField(default=timezone.now().date())
+    name = models.CharField(max_length=100, default="Event", help_text="Enter event name")
+    description = models.CharField(max_length=200, blank=True, null=True, help_text="Enter description")
+    localization = models.CharField(max_length=50, blank=True, null=True, help_text="Enter localization")
     status = models.CharField(choices=EVENT_STATUS, default="Preparing", max_length=20)
     end_date = models.DateField(blank=True, null=True)
 
