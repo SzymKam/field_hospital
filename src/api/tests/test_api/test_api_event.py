6decb6b103e2681c9e9b13c6cbf9bd86544b7afb
SzymKam <szymon15kaminski@gmail.com>
2023-08-31 17:04:00 +0200

    feat: test crud for api event list and detail view

--- src/api/tests/test_api/test_api_event.py
+++ src/api/tests/test_api/test_api_event.py
@@ -1,10 +1,12 @@
 import secrets
+from json import dumps
 
 from django.contrib.auth.models import Permission
 from django.test import TestCase, tag
 from django.urls import reverse
 from faker import Faker
 from rest_framework import status
+from rest_framework.exceptions import ErrorDetail
 
 from ..factories.event_factory import EventFactory
 from ..factories.user_factory import UserFactory
@@ -26,6 +28,7 @@ class TestEventResponse(TestCase):
             "description": fake.text(),
             "localization": fake.address(),
         }
+        self.headers = {"content_type": "application/json"}
 
     def test_list_get_not_logged_user_return_403(self):
         response = self.client.get(path=self.url_list)
@@ -49,7 +52,7 @@ class TestEventResponse(TestCase):
         self.assertEqual(response.data[1]["description"], self.event_2.description)
 
     def test_list_post_not_logged_user_return_403(self):
-        response = self.client.post(path=self.url_list, data=self.data)
+        response = self.client.post(path=self.url_list)
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.assertEqual(response.request["REQUEST_METHOD"], "POST")
@@ -60,7 +63,6 @@ class TestEventResponse(TestCase):
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.request["REQUEST_METHOD"], "POST")
-
         self.assertEqual(response.data["name"], self.data["name"])
         self.assertEqual(response.data["description"], self.data["description"])
         self.assertEqual(response.data["status"], self.data["status"])
@@ -84,14 +86,14 @@ class TestEventResponse(TestCase):
         self.assertEqual(response.request["REQUEST_METHOD"], "POST")
 
     def test_list_patch_not_logged_user_return_403(self):
-        response = self.client.patch(path=self.url_list, data=self.data)
+        response = self.client.patch(path=self.url_list)
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.assertEqual(response.request["REQUEST_METHOD"], "PATCH")
 
-    def test_list_patch_logged_user_return_403(self):
+    def test_list_patch_logged_user_return_405(self):
         self.client.force_login(user=self.user)
-        response = self.client.patch(path=self.url_list, data=self.data)
+        response = self.client.patch(path=self.url_list)
 
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
         self.assertEqual(response.request["REQUEST_METHOD"], "PATCH")
@@ -136,14 +138,79 @@ class TestEventResponse(TestCase):
         self.assertEqual(response.request["REQUEST_METHOD"], "GET")
 
     def test_detail_post_not_logged_user_return_403(self):
-        response = self.client.post(path=self.url_detail, data=self.data)
+        response = self.client.post(path=self.url_detail)
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.assertEqual(response.request["REQUEST_METHOD"], "POST")
 
     def test_detail_post_logged_user_return_405(self):
         self.client.force_login(user=self.user)
-        response = self.client.post(path=self.url_detail, data=self.data)
+        response = self.client.post(path=self.url_detail)
 
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
         self.assertEqual(response.request["REQUEST_METHOD"], "POST")
+
+    def test_detail_patch_not_logged_user_return_403(self):
+        response = self.client.patch(path=self.url_detail)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.request["REQUEST_METHOD"], "PATCH")
+
+    def test_detail_patch_logged_user_return_200(self):
+        self.client.force_login(user=self.user)
+
+        data = {"name": "updated_name", "status": "In progress"}
+
+        response = self.client.patch(path=self.url_detail, data=dumps(data), **self.headers)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.request["REQUEST_METHOD"], "PATCH")
+        self.assertEqual(response.data["id"], self.event_1.id)
+        self.assertEqual(response.data["name"], data["name"])
+        self.assertEqual(response.data["status"], data["status"])
+
+    def test_detail_patch_logged_user_invalid_data_return_400(self):
+        self.client.force_login(user=self.user)
+
+        data = {"status": "any_status"}
+
+        response = self.client.patch(path=self.url_detail, data=dumps(data), **self.headers)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.request["REQUEST_METHOD"], "PATCH")
+        self.assertEqual(
+            response.data["status"], [ErrorDetail(string='"any_status" is not a valid choice.', code="invalid_choice")]
+        )
+
+    def test_detail_patch_logged_user_invalid_data_return_404(self):
+        self.client.force_login(user=self.user)
+
+        data = {"status": "any_status"}
+
+        response = self.client.patch(
+            path=reverse("api-event-detail", kwargs={"pk": 99999}), data=dumps(data), **self.headers
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.request["REQUEST_METHOD"], "PATCH")
+        self.assertEqual(response.data["detail"], ErrorDetail(string="Not found.", code="not_found"))
+
+    def test_detail_delete_not_logged_user_return_403(self):
+        response = self.client.delete(path=self.url_detail)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.request["REQUEST_METHOD"], "DELETE")
+
+    def test_detail_delete_logged_user_return_204(self):
+        self.client.force_login(user=self.user)
+        response = self.client.delete(path=self.url_detail)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(response.request["REQUEST_METHOD"], "DELETE")
+
+    def test_detail_delete_logged_user_invalid_id_return_404(self):
+        self.client.force_login(user=self.user)
+        response = self.client.delete(path=reverse("api-event-detail", kwargs={"pk": 99999}))
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.request["REQUEST_METHOD"], "DELETE")
