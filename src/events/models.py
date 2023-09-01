7890e7f07142d4af5a4f9715b2a7fa869b23d4a5
SzymKam <szymon15kaminski@gmail.com>
2023-09-01 13:45:51 +0200

    fix: update event model

--- src/events/models.py
+++ src/events/models.py
@@ -8,7 +8,7 @@ class Event(models.Model):
     start_date = models.DateField(default=timezone.now().date())
     name = models.CharField(max_length=100, default="Event", help_text="Enter event name")
     description = models.CharField(max_length=200, blank=True, null=True, help_text="Enter description")
-    localization = models.CharField(max_length=50, blank=True, null=True, help_text="Enter localization")
+    localization = models.CharField(max_length=200, blank=True, null=True, help_text="Enter localization")
     status = models.CharField(choices=EVENT_STATUS, default="Preparing", max_length=20)
     end_date = models.DateField(blank=True, null=True)
 
