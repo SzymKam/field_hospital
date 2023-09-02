d3459e68f695b6a983ba40c0d840ac03d1ca090e
SzymKam <szymon15kaminski@gmail.com>
2023-09-02 11:02:27 +0200

    fix: change fields -> add id

--- src/api/serializers/drug_serializer.py
+++ src/api/serializers/drug_serializer.py
@@ -6,4 +6,4 @@ from treatment.models.drug_model import Drug
 class DrugSerializer(serializers.ModelSerializer):
     class Meta:
         model = Drug
-        fields = ["name", "dose", "unit", "dosage_form", "treatment"]
+        fields = ["name", "dose", "unit", "dosage_form", "treatment", "id"]
