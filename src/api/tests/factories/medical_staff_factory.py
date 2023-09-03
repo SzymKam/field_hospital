0a63828f61a71ee9ca18da1f4d81ea291b138837
SzymKam <szymon15kaminski@gmail.com>
2023-09-03 20:37:03 +0200

    fix: update medical staff factory, use faker from factory

--- src/api/tests/factories/medical_staff_factory.py
+++ src/api/tests/factories/medical_staff_factory.py
@@ -1,14 +1,12 @@
+from factory import Faker
 from factory.django import DjangoModelFactory
-from faker import Faker
 
 from treatment.models.medical_staff_model import MedicalStaff
-
-fake = Faker()
 
 
 class MedicalStaffFactory(DjangoModelFactory):
     class Meta:
         model = MedicalStaff
 
-    name = fake.first_name()
-    surname = fake.last_name()
+    name = Faker("first_name")
+    surname = Faker("last_name")
