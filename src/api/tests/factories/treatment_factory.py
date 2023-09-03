65d77e1432c62f485fe819e8f9486221a64724ba
SzymKam <szymon15kaminski@gmail.com>
2023-09-03 20:21:23 +0200

    fix: update treatment factory, use faker from factory

--- src/api/tests/factories/treatment_factory.py
+++ src/api/tests/factories/treatment_factory.py
@@ -1,21 +1,18 @@
-from factory import SubFactory
+from factory import Faker, SubFactory
 from factory.django import DjangoModelFactory
-from faker import Faker
 
 from treatment.models.treatment_model import Treatment
 
 from .medical_staff_factory import MedicalStaffFactory
 from .patient_factory import PatientFactory
 
-fake = Faker()
-
 
 class TreatmentFactory(DjangoModelFactory):
     class Meta:
         model = Treatment
 
-    interview = fake.text()
-    description = fake.text()
-    diagnosis = fake.name()
+    interview = Faker("sentence")
+    description = Faker("sentence")
+    diagnosis = Faker("word")
     medical_staff = SubFactory(MedicalStaffFactory)
-    patient = SubFactory(PatientFactory)  # FK one to one
+    patient = SubFactory(PatientFactory)
