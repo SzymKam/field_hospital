928f209ceaa48b3a551d0380dff3409de70d25e9
SzymKam <szymon15kaminski@gmail.com>
2023-09-03 20:36:31 +0200

    fix: update patient factory, use faker from factory

--- src/api/tests/factories/patient_factory.py
+++ src/api/tests/factories/patient_factory.py
@@ -1,23 +1,20 @@
-from factory import SubFactory
+from factory import Faker, SubFactory
 from factory.django import DjangoModelFactory
-from faker import Faker
 
 from patients.models import Patient
 
 from .event_factory import EventFactory
-
-fake = Faker()
 
 
 class PatientFactory(DjangoModelFactory):
     class Meta:
         model = Patient
 
-    name = fake.first_name()
-    surname = fake.last_name()
-    PESEL = fake.numerify(text="###########")
-    address = fake.address()
-    phone = fake.numerify(text="#########")
-    email = fake.email()
-    additional_info = fake.text()
+    name = Faker("first_name")
+    surname = Faker("last_name")
+    PESEL = Faker("pyint", min_value=10000000000, max_value=99999999999)
+    address = Faker("address")
+    phone = Faker("pyint", min_value=100000000, max_value=999999999)
+    email = Faker("email")
+    additional_info = Faker("sentence")
     event = SubFactory(EventFactory)
