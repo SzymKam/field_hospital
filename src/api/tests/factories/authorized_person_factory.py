dfeb584d2c0c9df1269450177278bc97b49a6e31
SzymKam <szymon15kaminski@gmail.com>
2023-09-03 19:50:47 +0200

    fix: update authorized person model factory - change creating fields

--- src/api/tests/factories/authorized_person_factory.py
+++ src/api/tests/factories/authorized_person_factory.py
@@ -1,15 +1,13 @@
+from factory import Faker
 from factory.django import DjangoModelFactory
-from faker import Faker
 
 from patients.models import AuthorizedPerson
-
-fake = Faker()
 
 
 class AuthorizedPersonFactory(DjangoModelFactory):
     class Meta:
         model = AuthorizedPerson
 
-    name = fake.first_name()
-    surname = fake.last_name()
-    phone = fake.numerify(text="#########")
+    name = Faker("first_name")
+    surname = Faker("last_name")
+    phone = Faker("random_int")
