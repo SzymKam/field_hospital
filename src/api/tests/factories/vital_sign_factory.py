cba15dde19bdb6146cf57a8c869083b717da3122
SzymKam <szymon15kaminski@gmail.com>
2023-09-03 19:31:30 +0200

    feat: add vital sign model factory for tests

---
+++ src/api/tests/factories/vital_sign_factory.py
@@ -0,0 +1,26 @@
+import random
+import secrets
+
+from factory import SubFactory
+from factory.django import DjangoModelFactory
+from faker import Faker
+
+from treatment.models.vital_sign_model import VitalSign
+
+from .treatment_factory import TreatmentFactory
+
+fake = Faker()
+
+
+class VitalSignFactory(DjangoModelFactory):
+    class Meta:
+        model = VitalSign
+
+    bp_sys = random.randint(1, 350)
+    bp_dia = random.randint(1, 350)
+    hr = random.randint(1, 350)
+    sao2 = random.randint(30, 100)
+    temperature = random.uniform(13.0, 45.0)
+    glycemia = random.randint(1, 900)
+    gcs = random.randint(3, 15)
+    treatment = SubFactory(TreatmentFactory)
