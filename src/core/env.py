5981e04b40e75082581da0220658825aa3c1d898
SzymKam <szymon15kaminski@gmail.com>
2023-09-12 19:33:34 +0200

    feat: add user and password into db

--- src/core/env.py
+++ src/core/env.py
@@ -4,4 +4,6 @@ from django.core.management.utils import get_random_secret_key
 env = environ.Env(
     DEBUG=(bool, False),
     SECRET_KEY=(str, get_random_secret_key()),
+    USER=(str, "field_hospital_user"),
+    PASSWORD=(str, "field_hospital_user"),
 )
