fb19b9762cbdcce7c75e9db59832501815e1b90c
SzymKam <szymon15kaminski@gmail.com>
2023-09-12 19:33:59 +0200

    feat: add new db - postgresql

--- src/core/settings.py
+++ src/core/settings.py
@@ -97,13 +97,22 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "field_hospital_db",
+        "USER": env("USER"),
+        "PASSWORD": env("PASSWORD"),
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
