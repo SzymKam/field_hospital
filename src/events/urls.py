29d6d9c3df26e4e49de79761ef485da528519baf
SzymKam <szymon15kaminski@gmail.com>
2023-09-16 20:57:45 +0200

    fix: add use static files into urls

--- src/events/urls.py
+++ src/events/urls.py
@@ -1,28 +1,32 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from .views import (
     AllActiveEventView,
     AllInactiveEventView,
+    CloseRestoreEventView,
     CreateEventView,
+    DeleteEventView,
     DetailEventView,
     UpdateEventView,
-    DeleteEventView,
-    CloseRestoreEventView,
 )
 
-urlpatterns = [
-    path("", AllActiveEventView.as_view(), name="all-events"),
-    path("events/", CreateEventView.as_view(), name="add-events"),
-    path("events/<int:pk>", DetailEventView.as_view(), name="detail-events"),
-    path("events/<int:pk>/update", UpdateEventView.as_view(), name="update-events"),
-    path("events/inactive/", AllInactiveEventView.as_view(), name="complete-events"),
-    path("events/<int:pk>/delete", DeleteEventView.as_view(), name="delete-events"),
-    path(
-        "events/<int:pk>/close", CloseRestoreEventView.close_event, name="close-events"
-    ),
-    path(
-        "events/<int:pk>/restore",
-        CloseRestoreEventView.restore_event,
-        name="restore-events",
-    ),
-]
+urlpatterns = (
+    [
+        path("", AllActiveEventView.as_view(), name="all-events"),
+        path("events/", CreateEventView.as_view(), name="add-events"),
+        path("events/<int:pk>", DetailEventView.as_view(), name="detail-events"),
+        path("events/<int:pk>/update", UpdateEventView.as_view(), name="update-events"),
+        path("events/inactive/", AllInactiveEventView.as_view(), name="complete-events"),
+        path("events/<int:pk>/delete", DeleteEventView.as_view(), name="delete-events"),
+        path("events/<int:pk>/close", CloseRestoreEventView.close_event, name="close-events"),
+        path(
+            "events/<int:pk>/restore",
+            CloseRestoreEventView.restore_event,
+            name="restore-events",
+        ),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
--- src/patients/urls.py
+++ src/patients/urls.py
@@ -1,41 +1,48 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
+
+from patients.views.auth_person_view import CreateAuthPersonView, UpdateAuthPersonView
 from patients.views.patients_views import (
     CreatePatientView,
     DetailPatientView,
-    UpdatePatientView,
     DischargePatientView,
+    UpdatePatientView,
 )
-from patients.views.auth_person_view import CreateAuthPersonView, UpdateAuthPersonView
 
-urlpatterns = [
-    path(
-        "events/<int:event>/patients",
-        CreatePatientView.create_patient,
-        name="add-patient",
-    ),
-    path(
-        "events/<int:event>/patients/<int:pk>",
-        DetailPatientView.as_view(),
-        name="detail-patient",
-    ),
-    path(
-        "events/<int:event>/patients/<int:pk>/update",
-        UpdatePatientView.as_view(),
-        name="update-patient",
-    ),
-    path(
-        "events/<int:event>/patients/<int:pk>/discharge",
-        DischargePatientView.discharge_patient,
-        name="discharge-patient",
-    ),
-    path(
-        "events/<int:event>/patients/<int:pk>/auth-person",
-        CreateAuthPersonView.as_view(),
-        name="add-auth-person",
-    ),
-    path(
-        "events/<int:event>/patients/<int:patient>/auth-person/<int:pk>/update",
-        UpdateAuthPersonView.as_view(),
-        name="update-auth-person",
-    ),
-]
+urlpatterns = (
+    [
+        path(
+            "events/<int:event>/patients",
+            CreatePatientView.create_patient,
+            name="add-patient",
+        ),
+        path(
+            "events/<int:event>/patients/<int:pk>",
+            DetailPatientView.as_view(),
+            name="detail-patient",
+        ),
+        path(
+            "events/<int:event>/patients/<int:pk>/update",
+            UpdatePatientView.as_view(),
+            name="update-patient",
+        ),
+        path(
+            "events/<int:event>/patients/<int:pk>/discharge",
+            DischargePatientView.discharge_patient,
+            name="discharge-patient",
+        ),
+        path(
+            "events/<int:event>/patients/<int:pk>/auth-person",
+            CreateAuthPersonView.as_view(),
+            name="add-auth-person",
+        ),
+        path(
+            "events/<int:event>/patients/<int:patient>/auth-person/<int:pk>/update",
+            UpdateAuthPersonView.as_view(),
+            name="update-auth-person",
+        ),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
