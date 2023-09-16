018f76fb5eff8b7a69f47469a5b13e022fe532c1
SzymKam <szymon15kaminski@gmail.com>
2023-09-16 20:54:47 +0200

    fix: change into debug True value

--- src/core/env.py
+++ src/core/env.py
@@ -2,8 +2,9 @@ import environ
 from django.core.management.utils import get_random_secret_key
 
 env = environ.Env(
-    DEBUG=(bool, False),
+    DEBUG=(bool, True),
     SECRET_KEY=(str, get_random_secret_key()),
     USER=(str, "field_hospital_user"),
     PASSWORD=(str, "field_hospital_user"),
+    HOST=(str, "db"),
 )
