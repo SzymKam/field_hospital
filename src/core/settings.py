15fa9016bcea6ecd3f89a99555252958909cf5f3
SzymKam <szymon15kaminski@gmail.com>
2023-09-16 20:56:09 +0200

    fix: add static url, and media url

--- src/core/settings.py
+++ src/core/settings.py
@@ -97,19 +97,13 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "field_hospital_db",
         "USER": env("USER"),
         "PASSWORD": env("PASSWORD"),
-        "HOST": "127.0.0.1",
+        "HOST": env("HOST"),
         "PORT": "5432",
     }
 }
@@ -148,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -158,6 +152,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 LOGIN_REDIRECT_URL = "all-events"
 LOGOUT_REDIRECT_URL = "user-login"
