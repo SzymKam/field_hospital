a381cffe2cbad768a0eb8790d92e7adaff7a6756
SzymKam <szymon15kaminski@gmail.com>
2023-09-19 09:56:48 +0200

    feat: add validation of bed and priority choice when create and update patient by api

--- src/api/views/patient_view.py
+++ src/api/views/patient_view.py
@@ -1,8 +1,11 @@
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from api.serializers.patient_serializer import PatientSerializer
 from patients.models import Patient
+
+from ..constants import ALLOWED_BED_CHOICE, ALLOWED_PRIORITY_CHOICE
 
 
 class PatientViewSet(ModelViewSet):
@@ -16,3 +19,27 @@ class PatientViewSet(ModelViewSet):
         if event:
             queryset = queryset.filter(event=event)
         return queryset
+
+    def perform_create(self, serializer):
+        bed_number = serializer.validated_data.get("bed_number")
+        priority = serializer.validated_data.get("priority")
+        allowed_bed_number = ALLOWED_BED_CHOICE
+        allowed_priority = ALLOWED_PRIORITY_CHOICE
+        if bed_number not in allowed_bed_number:
+            raise ValueError("Invalid bed number")
+        if priority not in allowed_priority:
+            raise ValueError("Invalid priority")
+        serializer.save()
+
+    def perform_update(self, serializer):
+        if "bed_number" in serializer.validated_data.keys():
+            bed_number = serializer.validated_data.get("bed_number")
+            allowed_bed_number = ALLOWED_BED_CHOICE
+            if bed_number not in allowed_bed_number:
+                raise ValidationError("Invalid bed number choice")
+        if "priority" in serializer.validated_data.keys():
+            priority = serializer.validated_data.get("priority")
+            allowed_priority = ALLOWED_PRIORITY_CHOICE
+            if priority not in allowed_priority:
+                raise ValidationError("Invalid priority choice")
+        serializer.save()
