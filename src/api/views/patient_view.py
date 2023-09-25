653a31532e6c5daf27122c5db13283f2a35edac9
SzymKam <szymon15kaminski@gmail.com>
2023-09-25 23:23:42 +0200

    fix: delete unused imports

--- src/api/views/patient_view.py
+++ src/api/views/patient_view.py
@@ -4,8 +4,6 @@ from rest_framework.viewsets import ModelViewSet
 from api.serializers.patient_serializer import PatientSerializer
 from patients.models import Patient
 
-from ..constants import ALLOWED_BED_CHOICE, ALLOWED_PRIORITY_CHOICE
-
 
 class PatientViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
