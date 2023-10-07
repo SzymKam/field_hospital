a8e3f54819b104120e9fff433c3af666cd79dbe3
SzymKam <szymon15kaminski@gmail.com>
2023-10-08 00:54:19 +0200

    fix: change path of apps -> remove 'src/' for local server

--- src/core/settings.py
+++ src/core/settings.py
@@ -56,11 +56,11 @@ INSTALLED_APPS = [
 ]
 
 INSTALLED_EXTENSIONS = [
-    "src.events",
-    "src.patients",
-    "src.treatment",
-    "src.users",
-    "src.api",
+    "events",
+    "patients",
+    "treatment",
+    "users",
+    "api",
 ]
 
 INSTALLED_APPS += INSTALLED_EXTENSIONS
@@ -76,7 +76,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "src.core.urls"
+ROOT_URLCONF = "core.urls"
 
 TEMPLATES = [
     {
@@ -100,21 +100,14 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql_psycopg2",
-#         "NAME": "field_hospital_db",
-#         "USER": env("USER"),
-#         "PASSWORD": env("PASSWORD"),
-#         "HOST": env("HOST"),
-#         "HOST": "127.0.0.1",
-#         "PORT": "5432",
-#     }
-# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "field_hospital_db",
+        "USER": env("USER"),
+        "PASSWORD": env("PASSWORD"),
+        "HOST": env("HOST"),
+        "PORT": "5432",
     }
 }
 
