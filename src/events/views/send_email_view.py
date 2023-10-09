6f661bb1e5ab42d164058424a0ea0b1667321deb
SzymKam <szymon15kaminski@gmail.com>
2023-10-09 23:35:06 +0200

    fix/feat: reformat and improve send email class

--- src/events/views/send_email_view.py
+++ src/events/views/send_email_view.py
@@ -1,9 +1,8 @@
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.mail import send_mail
 from django.shortcuts import get_list_or_404, get_object_or_404, redirect, render
 from django.template.loader import render_to_string
-from django.urls import reverse_lazy
+from django.views import View
 
 from core.settings import DEFAULT_FROM_EMAIL
 from patients.models import Patient
@@ -12,45 +11,48 @@ from ..forms.email_choice_form import EmailSelectionForm
 from ..models import Event
 
 
-class EmailFlow:
-    @staticmethod
-    def get_recipient(request, pk):
-        event = get_object_or_404(klass=Event, pk=pk)
-        users_list = get_list_or_404(klass=User)
-        form = EmailSelectionForm
+class EmailFlowView(View, LoginRequiredMixin):
+    template_name = "events/email-recipient.html"
+
+    def get(self, request, event_pk):
+        event = get_object_or_404(Event, pk=event_pk)
+        form = EmailSelectionForm()
         return render(
             request,
-            "events/email-recipient.html",
-            {"title": "Email recipient", "users_list": users_list, "event": event, "form": form},
+            self.template_name,
+            {"title": "Email recipient", "event": event, "form": form},
         )
 
-
-def select_email(request):
-    if request.method == "POST":
+    def post(self, request, event_pk):
+        event = get_object_or_404(Event, pk=event_pk)
         form = EmailSelectionForm(request.POST)
+
         if form.is_valid():
-            selected_email = form.cleaned_data["email"]
-            # Do something with the selected email here, e.g., save it to a model.
-    else:
-        form = EmailSelectionForm()
+            selected_user_email = form.cleaned_data["email"]
+            self._send_event_detail(request, event, selected_user_email)
+            return redirect("detail-events", pk=event_pk)
+        return render(
+            request,
+            self.template_name,
+            {"title": "Email recipient", "event": event, "form": form},
+        )
 
-    return render(request, "your_template.html", {"form": form})
+    def _send_event_detail(self, request, event, selected_user_email):
+        patients_list = get_list_or_404(klass=Patient, event=event)
+        template_name = "events/email-template.html"
+        subject = f"Patient list of {event.name}"
+        context = {"patients_list": patients_list, "event": event}
+        message = render_to_string(template_name, context)
 
+        from_email = DEFAULT_FROM_EMAIL
+        recipient_list = [
+            selected_user_email.email,
+        ]
 
-@login_required()
-def send_patient_list(request, pk):
-    event = get_object_or_404(klass=Event, pk=pk)
-    patients_list = get_list_or_404(klass=Patient, event=event)
-    template_name = "events/email-template.html"
-    subject = f"Patient list of {event.name}"
-    context = {subject: "suka"}
-    message = render_to_string(template_name, context)
-
-    from_email = DEFAULT_FROM_EMAIL
-    recipient_list = [
-        "szymon15kaminski@gmail.com",
-    ]
-
-    send_mail(subject=subject, message=message, from_email=from_email, recipient_list=recipient_list)
-
-    return redirect("detail-events", pk=pk)
+        send_mail(
+            subject=subject,
+            message="",
+            from_email=from_email,
+            recipient_list=recipient_list,
+            html_message=message,
+        )
