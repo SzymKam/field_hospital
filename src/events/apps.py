b53c4da8554eda718501ca0f68674a4669403950
SzymKam <szymon15kaminski@gmail.com>
2023-10-12 14:55:29 +0200

    feat: add auto-create group when migrations

--- src/events/apps.py
+++ src/events/apps.py
@@ -1,6 +1,23 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
 
 class EventsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "events"
+
+    def ready(self):
+        post_migrate.connect(self._create_initial_staff_admin_group, sender=self)
+
+    def _create_initial_staff_admin_group(self, sender, **kwargs):
+        from django.contrib.auth.models import Group, Permission
+
+        permission_add_user = Permission.objects.get(codename="add_user")
+        permission_change_user = Permission.objects.get(codename="change_user")
+        permission_delete_user = Permission.objects.get(codename="delete_user")
+
+        user_group, created = Group.objects.get_or_create(name="User admin group")
+        print("x")
+        if created:
+            user_group.permissions.add(permission_add_user, permission_change_user, permission_delete_user)
+            user_group.save()
