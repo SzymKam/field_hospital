953c6b2bf0f1666eb9bde50de9dca720f0a426bd
SzymKam <szymon15kaminski@gmail.com>
2023-10-22 19:28:32 +0200

    fix: change env variables

--- src/core/env.py
+++ src/core/env.py
@@ -4,7 +4,11 @@ from django.core.management.utils import get_random_secret_key
 env = environ.Env(
     DEBUG=(bool, True),
     SECRET_KEY=(str, get_random_secret_key()),
-    USER=(str, "field_hospital_user"),
-    PASSWORD=(str, "field_hospital_user"),
+    USER=(str, "db_field_hospital"),
+    PASSWORD=(str, "db_field_hospital"),
+    NAME=(str, "db_field_hospital"),
     HOST=(str, "db"),
+    EMAIL_HOST_PASSWORD=(str, ""),
+    EMAIL_HOST_USER=(str, ""),
+    DEFAULT_FROM_EMAIL=(str, ""),
 )
