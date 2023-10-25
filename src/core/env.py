9fcd7fa82e288d46744b0b8b7c6651c93dca48fa
SzymKam <szymon15kaminski@gmail.com>
2023-10-25 21:23:14 +0200

    fix: update file

--- src/core/env.py
+++ src/core/env.py
@@ -2,13 +2,13 @@ import environ
 from django.core.management.utils import get_random_secret_key
 
 env = environ.Env(
-    DEBUG=(bool, True),
+    DEBUG=(bool, False),
     SECRET_KEY=(str, get_random_secret_key()),
     USER=(str, "db_field_hospital"),
     PASSWORD=(str, "db_field_hospital"),
     NAME=(str, "db_field_hospital"),
     HOST=(str, "db"),
-    EMAIL_HOST_PASSWORD=(str, ""),
-    EMAIL_HOST_USER=(str, ""),
-    DEFAULT_FROM_EMAIL=(str, ""),
+    EMAIL_HOST_PASSWORD=(str, None),
+    EMAIL_HOST_USER=(str, None),
+    DEFAULT_FROM_EMAIL=(str, None),
 )
