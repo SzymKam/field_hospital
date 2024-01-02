904d7fb4832897ddda7f1fbb9726c811a05595bb
SzymKam <szymon15kaminski@gmail.com>
2024-01-02 15:32:46 +0100

    fix: working on aws s3 -> now connect and reading staticfiles

--- src/core/settings.py
+++ src/core/settings.py
@@ -179,16 +179,14 @@ DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
 
 
 """AWS S3 settings"""
-# USE_S3 = os.getenv('USE_S3') == 'TRUE'
-#
-# if USE_S3:
-# aws settings
+
 AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
 AWS_DEFAULT_ACL = "public-read"
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+
 # s3 static settings
 AWS_LOCATION = "static"
 STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
