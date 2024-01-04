f59bf59307422a4c7f2204e76607f93c4bc0ad87
SzymKam <szymon15kaminski@gmail.com>
2024-01-04 10:37:47 +0100

    fix: update allowed hosts for new domain

--- src/core/settings.py
+++ src/core/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "fieldhospital.eu-central-1.elasticbeanstalk.com "]
 
 SITE_ID = 1
 
