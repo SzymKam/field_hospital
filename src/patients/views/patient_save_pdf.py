da488c74bd3f29be3c63c0e0e8bc4b6ff58233a7
SzymKam <szymon15kaminski@gmail.com>
2024-01-05 00:35:55 +0100

    fix: fixing error with render image in pdf file

--- src/patients/views/patient_save_pdf.py
+++ src/patients/views/patient_save_pdf.py
@@ -1,4 +1,8 @@
+import os
+
+from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.staticfiles import finders
 from django.db.models.query import QuerySet
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404
@@ -15,6 +19,35 @@ from ..models import Event
 
 
 class PDFPatientView(View, LoginRequiredMixin):
+    def link_callback(self, uri, rel):
+        """
+        Convert HTML URIs to absolute system paths so xhtml2pdf can access those
+        resources
+        """
+        result = finders.find(uri)
+        if result:
+            if not isinstance(result, (list, tuple)):
+                result = [result]
+            result = list(os.path.realpath(path) for path in result)
+            path = result[0]
+        else:
+            sUrl = settings.STATIC_URL  # Typically /static/
+            sRoot = settings.STATIC_ROOT  # Typically /home/userX/project_static/
+            mUrl = settings.MEDIA_URL  # Typically /media/
+            mRoot = settings.MEDIA_ROOT  # Typically /home/userX/project_static/media/
+
+            if uri.startswith(mUrl):
+                path = os.path.join(mRoot, uri.replace(mUrl, ""))
+            elif uri.startswith(sUrl):
+                path = os.path.join(sRoot, uri.replace(sUrl, ""))
+            else:
+                return uri
+
+        # make sure that file exists
+        if not os.path.isfile(path):
+            raise RuntimeError("media URI must start with %s or %s" % (sUrl, mUrl))
+        return path
+
     def get(self, request: HttpRequest, event_pk: int, patient_pk: int):
         event = get_object_or_404(Event, pk=event_pk)
         patient = get_object_or_404(Patient, pk=patient_pk)
@@ -26,23 +59,41 @@ class PDFPatientView(View, LoginRequiredMixin):
             request=request, event=event, patient=patient, drugs=drugs, vital_signs=vital_signs
         )
 
-    @staticmethod
     def _render_pdf_view(
-        request: HttpRequest, event: Event, patient: Patient, drugs: QuerySet[Drug], vital_signs: QuerySet[VitalSign]
+        self,
+        request: HttpRequest,
+        event: Event,
+        patient: Patient,
+        drugs: QuerySet[Drug],
+        vital_signs: QuerySet[VitalSign],
     ):
         template_path = "patients/patient-detail-pdf.html"
-        template = get_template(template_path)
 
         context = {"patient": patient, "event": event, "drugs": drugs, "vital_signs": vital_signs}
+
+        response = HttpResponse(content_type="application/pdf")
+        response["Content-Disposition"] = f'attachment; filename="{patient.surname} {patient.name}-detail.pdf"'
+
+        template = get_template(template_path)
         html = template.render(context)
 
-        pdf_file = HttpResponse(content_type="application/pdf")
-        pdf_file["Content-Disposition"] = f'attachment; filename="{patient.surname} {patient.name}-detail.pdf"'
-
-        base_url = request.build_absolute_uri("/")
-        pisa_status = pisa.CreatePDF(html, dest=pdf_file, link_callback=lambda uri, _: f"{base_url}{uri}")
+        pisa_status = pisa.CreatePDF(html, dest=response)
 
         if pisa_status.err:
-            return HttpResponse("Error generating PDF", status=500)
+            return HttpResponse("We had some errors <pre>" + html + "</pre>")
+        return response
 
-        return pdf_file
+        # template = get_template(template_path)
+
+        # html = template.render(context)
+
+        # pdf_file = HttpResponse(content_type="application/pdf")
+        # pdf_file["Content-Disposition"] = f'attachment; filename="{patient.surname} {patient.name}-detail.pdf"'
+
+        # base_url = request.build_absolute_uri("/")
+        # pisa_status = pisa.CreatePDF(html, dest=pdf_file, link_callback=lambda uri, _: f"{base_url}{uri}")
+        #
+        # if pisa_status.err:
+        #     return HttpResponse("Error generating PDF", status=500)
+        #
+        # return pdf_file
