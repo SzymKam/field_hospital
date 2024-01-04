a546ff6a074dce5cc0c50c283e9e31022de4b112
SzymKam <szymon15kaminski@gmail.com>
2024-01-05 00:39:52 +0100

    fix: delete usused link_callback method

--- src/patients/views/patient_save_pdf.py
+++ src/patients/views/patient_save_pdf.py
@@ -19,35 +19,6 @@ from ..models import Event
 
 
 class PDFPatientView(View, LoginRequiredMixin):
-    def link_callback(self, uri, rel):
-        """
-        Convert HTML URIs to absolute system paths so xhtml2pdf can access those
-        resources
-        """
-        result = finders.find(uri)
-        if result:
-            if not isinstance(result, (list, tuple)):
-                result = [result]
-            result = list(os.path.realpath(path) for path in result)
-            path = result[0]
-        else:
-            sUrl = settings.STATIC_URL  # Typically /static/
-            sRoot = settings.STATIC_ROOT  # Typically /home/userX/project_static/
-            mUrl = settings.MEDIA_URL  # Typically /media/
-            mRoot = settings.MEDIA_ROOT  # Typically /home/userX/project_static/media/
-
-            if uri.startswith(mUrl):
-                path = os.path.join(mRoot, uri.replace(mUrl, ""))
-            elif uri.startswith(sUrl):
-                path = os.path.join(sRoot, uri.replace(sUrl, ""))
-            else:
-                return uri
-
-        # make sure that file exists
-        if not os.path.isfile(path):
-            raise RuntimeError("media URI must start with %s or %s" % (sUrl, mUrl))
-        return path
-
     def get(self, request: HttpRequest, event_pk: int, patient_pk: int):
         event = get_object_or_404(Event, pk=event_pk)
         patient = get_object_or_404(Patient, pk=patient_pk)
